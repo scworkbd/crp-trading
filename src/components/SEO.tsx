38e95892208259b1c98ee6b37ea395edd31d1caa
scworkbd <websitework155@outlook.com>
2022-11-24 00:29:47 +0600

    PL

--- src/components/SEO.tsx
+++ src/components/SEO.tsx
@@ -2,10 +2,10 @@ import Head from "next/head"
 import React from "react"
 
 const SEO = () => {
-  const _title = `CRP Trending`
-  const _description = "Earn money online, doing simple websites"
-  const _keywords = "CRP Trending"
-  const _author = "CRP Trending"
+  const _title = `Tyler Online`
+  const _description = "Tyle Online, Easy way to earn money online"
+  const _keywords = "tyler online"
+  const _author = "Tyler Online"
 
   return (
     <Head>
@@ -21,7 +21,7 @@ const SEO = () => {
       <meta property="og:image" content="/logo.png" />
       <meta property="og:image:secure_url" content="/logo.png" />
       <meta property="og:image:type" content="image/png" />
-      <meta property="og:image:alt" content="crp trending" />
+      <meta property="og:image:alt" content="tyler online" />
     </Head>
   )
 }
